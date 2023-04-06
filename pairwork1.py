cd15022b1ca6b7e0274eaf2775d887597a09cdf1
MethiyeSahin <methiyesahin@gmail.com>
2023-04-07 00:13:46 +0300

    pairwork1

---
+++ pairwork1.py
@@ -0,0 +1,24 @@
+
+#deneme1
+sayi1=int(input("bir sayi giriniz"))
+sayi2=int(input("bir sayi giriniz"))
+islem=input("bir islem seciniz")
+
+if islem== "+":
+     toplam=sayi1+sayi2
+elif islem=="-":
+      toplam=sayi1-sayi2
+elif islem== "*":
+      toplam=sayi1*sayi2
+elif islem=="/":
+      toplam=sayi1/sayi2
+else :
+    toplam=print("yanlıs islem sectiniz")
+
+print(toplam)
+#deneme1 bitiş
+
+
+
+
+
