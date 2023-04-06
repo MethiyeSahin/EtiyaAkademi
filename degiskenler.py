49693af8245a0e054b28b0c8eb3813f03655c1d7
MethiyeSahin <methiyesahin@gmail.com>
2023-04-06 23:14:24 +0300

    Assignment1

---
+++ degiskenler.py
@@ -0,0 +1,21 @@
+baslik="Haberiniz Olsun"
+vade=12
+faizorani=1.47
+
+# typing-start
+print(type(baslik))
+print(type(vade))
+# typing-start
+
+
+# karşılama-start
+mesaj="Hoşgeldin"
+MüsteriAdi="Methiye"
+MüsteriSoyadi="Şahin Taş"
+sonucmesaj=mesaj + " " +MüsteriAdi+" " +MüsteriSoyadi
+print(sonucmesaj)
+# karşılama-end
+
+sayi1=10
+sayi2=20
+print(sayi1+sayi2)
---
+++ listeler.py
@@ -0,0 +1,10 @@
+krediler = ["Hizli Kredi","Maaşini Halkbank'tan alanlara özel","Mutlu emekli ihtiyaç kredisi"]
+print(krediler)    #kredilistele
+print(krediler[0]) 
+print(krediler[1]) 
+print(krediler[2]) 
+print(len(krediler))
+
+krediler[0]="Çabuk kredi"
+print(krediler)
+print(krediler[2])
