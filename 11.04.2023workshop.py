a924ed38164e83a16c6246e57623fd12e4c2d1dc
MethiyeSahin <methiyesahin@gmail.com>
2023-04-11 12:03:58 +0300

    11.04.2023workshop

---
+++ 11.04.2023workshop.py
@@ -0,0 +1,19 @@
+class Human():
+    
+#self=> this
+    def __init__(self,name):
+        self.name= name
+    def talk(self,sentence):
+        print(f"{self.name}:{sentence}")
+    
+    def walk(self):
+        print(f"{self.name} is walking")
+
+human1=Human("Methiye")
+human1.talk("Merhaba")
+human1.walk()
+
+human2=Human("Merve")
+human2.talk("Selam")
+human2.walk()
+    
